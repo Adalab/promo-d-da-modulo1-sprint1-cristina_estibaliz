aa41bf6c7ab998387889633ea87825739575aa5d
esgadom <estgadom@gmail.com>
2023-02-28 13:08:32 +0100

    Resubida ejercicio insercion sql

--- sql-insercion-datos.sql
+++ sql-insercion-datos.sql
@@ -64,7 +64,59 @@ ALTER TABLE clientes
     CHECK ((length(codigo_postal) = 5));
     
 ALTER TABLE facturas
-	ADD COLUMN total FLOAT;meple
+	ADD COLUMN total FLOAT;
+    
+INSERT INTO zapatillas(
+	modelo, color, marca, talla)
+    VALUES ('XQYUN', 'negro', 'Nike', 42),
+			('UOPMN', 'rosas', 'Nike', 39),
+            ('OPYNT', 'verdes', 'Adidas', 35);
+
+INSERT INTO empleados(
+	nombre, tienda, salario, fecha_incorporacion)
+    VALUES ('Laura', 'Alcobendas', 25987, '2010-09-03'),
+			('Maria', 'Sevilla', NULL, '2001-04-11'),
+            ('Esther', 'Oviedo', 30165.98, '2000-11-29');
+            
+INSERT INTO clientes(
+	nombre, numero_telefono, email, direccion, ciudad, provincia, codigo_postal)
+    VALUES ('Monica', 1234567289, 'monica@email.com', 'Calle Felicidad', 'Mostoles', 'Madrid', 28176),
+			('Lorena', 289345678, 'lorena@email.com', 'Calle Alegria', 'Barcelona', 'Barcelona', 12346),
+            ('Carmen', 298463759, 'carmen@email.com', 'Calle del Color', 'Vigo', 'Pontevedra', 23456);
+            
+INSERT INTO facturas(
+	numero_factura, fecha, id_empleados, id_cliente, id_zapatilla, total)
+    VALUES (123, '2001-12-11', 2, 1, 1, 54.98),
+			(1234, '2005-05-23', 1, 3, 1, 89.91),
+            (12345, '2015-09-18', 3, 3, 2, 76.23);
+            
+UPDATE zapatillas
+	SET color = 'amarillo'
+    WHERE id_zapatilla = 2;
+    
+    
+UPDATE clientes
+	SET numero_telefono = 123456728
+    WHERE id_cliente = 1;
+    
+UPDATE facturas
+	SET total = 89.91
+    WHERE id_zapatilla = 2;
+
+
+UPDATE empleados
+	SET tienda = 'La Coruña'
+    WHERE id_empleados = 1;
+    
+    
+    
+    
+    
+
+    
+    
+            
+	
 
     
     
