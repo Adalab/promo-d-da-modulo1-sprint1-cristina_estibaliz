aa92075229f65778ae7a7ba0437fe8f053a1f947
esgadom <estgadom@gmail.com>
2023-03-03 13:06:35 +0100

    Hasta punto 6

--- ejercicios_consultas_basicas2.sql
+++ ejercicios_consultas_basicas2.sql
@@ -4,20 +4,31 @@
 **Gracias al uso de estos operadores seréis capaces de seleccionar información mucho más específica de la base de datos, 
 **la cual nos va a servir para realizar un análisis más en profundidad del negocio de la empresa.*/
 
+
 /*EJERCICIOS
 
 1. Conociendo el tipo de productos que vendemos en Northwind:
 Crea una consulta que muestre los primeros 10 productos según su ID y que nos indique el nombre de dichos productos y sus precios.*/
 
+SELECT product_name, unit_price, product_id
+	FROM products
+    LIMIT 10;
 
 /*2. Ordenando los resultados:
 Ahora realiza la misma consulta pero que nos muestre los últimos 10 productos según su ID de manera descendiente.*/
 
+SELECT product_name, unit_price, product_id
+	FROM products
+    ORDER BY product_id DESC
+    LIMIT 10;
+    
 
 /*3. Que pedidos tenemos en nuestra BBDD:
 Últimamente ha habido algo de descontrol en la empresa a la hora de controlar los pedidos. 
 Nos interesa conocer qué pedidos distintos hemos tenido (eliminando entradas duplicadas según su ID en la tabla order_details).*/
 
+SELECT DISTINCT order_id
+	FROM order_details;
 
 /*4. Los dos primeros pedidos:
 Como el número de pedidos es demasiado alto para visualizarlo de manera práctica en la consulta anterior, 
@@ -25,18 +36,30 @@ vamos a limitar los resultados sólo a los 2 primeros pedidos para verlos más e
 Sin embargo, en la tabla OrderDetails pueden existir varios registros para cada pedido, 
 por lo que no nos servirá con un uso normal del comando LIMIT.*/
 
+SELECT DISTINCT order_id
+	FROM order_details
+    LIMIT 2;
 
 /*5. Qué pedidos han gastado más:
 Una vez hemos inspeccionado el tipo de pedidos que tenemos en la empresa, desde la dirección nos piden conocer los 3 
 pedidos que han supuesto un mayor coste económico total para la empresa. Calcúlalo y dale el alias ImporteTotal. 
 Nota: Utiliza unit_price y quantity para calcular el importe total.*/
 
+SELECT (unit_price * quantity)
+AS ImporteTotal
+	FROM order_details
+    ORDER BY ImporteTotal DESC
+    LIMIT 3;
 
 
 /*6. Los pedidos que están entre las posiciones 5 y 10 de nuestro ranking:
 Ahora, no sabemos bien por qué razón, desde el departamento de Ventas nos piden seleccionar el ID de 
 los pedidos situados entre la 5 y la 10 mejor posición en cuanto al coste económico total ImporteTotal.*/
 
+SELECT (unit_price * quantity)
+	FROM ImporteTotal
+    WHERE order_id BETWEEN 4 AND 11;
+    
 
 /*7. Qué categorías tenemos en nuestra BBDD:
 De cara a ver cómo de diversificado está el negocio, se nos solicita una lista de las categorías que componen los 
